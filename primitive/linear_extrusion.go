f2efce9bfe1646893d8d073419cec6920bbc36c5
0daryo <plm23.ccc@gmail.com>
2022-12-11 17:33:34 +0900

    fix: render

--- primitive/linear_extrusion.go
+++ primitive/linear_extrusion.go
@@ -19,7 +19,6 @@ type LinearExtrusion struct {
 	Fn        uint16  "optional"
 	Items     *List   "forward:Add"
 	prefix    string  "prefix"
-	NoOption  bool
 }
 
 func NewLinearExtrusion(height float64, items ...Primitive) *LinearExtrusion {
@@ -39,10 +38,9 @@ func NewLinearExtrusion(height float64, items ...Primitive) *LinearExtrusion {
 
 func NewZeroLinearExtrusion(height float64, items ...Primitive) *LinearExtrusion {
 	ret := &LinearExtrusion{
-		Height:   height,
-		Scale:    1.0,
-		Items:    NewList(),
-		NoOption: true,
+		Height: height,
+		Scale:  1.0,
+		Items:  NewList(),
 	}
 	ret.Items.SetParent(ret)
 	ret.Items.Add(items...)
@@ -53,22 +51,22 @@ func (o *LinearExtrusion) Render(w *bufio.Writer) {
 	w.WriteString(o.Prefix())
 	w.WriteString("linear_extrude(")
 	w.WriteString(fmt.Sprintf("height=%f", o.Height))
-	if !o.NoOption {
+	if o.Center {
 		w.WriteString(fmt.Sprintf(", center=%t", o.Center))
 	}
-	if !o.NoOption {
+	if o.Convexity != 0 {
 		w.WriteString(fmt.Sprintf(", convexity=%d", o.Convexity))
 	}
-	if !o.NoOption {
+	if o.Twist != 0 {
 		w.WriteString(fmt.Sprintf(", twist=%d", o.Twist))
 	}
-	if !o.NoOption {
+	if o.Slices != 0 {
 		w.WriteString(fmt.Sprintf(", slices=%d", o.Slices))
 	}
-	if !o.NoOption {
+	if o.Scale != 0 {
 		w.WriteString(fmt.Sprintf(", scale=%f", o.Scale))
 	}
-	if !o.NoOption {
+	if o.Fn != 0 {
 		w.WriteString(fmt.Sprintf(", $fn=%d", o.Fn))
 	}
 	w.WriteString(")")
