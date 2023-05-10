e65e30e3d9709b2aae0ddd03c27e4a33efac5e67
0daryo <plm23.ccc@gmail.com>
2023-05-10 22:14:44 +0900

    fix: zero delta

--- primitive/offset.go
+++ primitive/offset.go
@@ -10,11 +10,12 @@ import (
 
 type Offset struct {
 	ParentImpl
-	R       float64 "optional"
-	Delta   float64 "optional"
-	Chamfer bool    "optional"
-	Items   *List   "forward:Add"
-	prefix  string  "prefix"
+	R         float64 "optional"
+	Delta     float64 "optional"
+	Chamfer   bool    "optional"
+	Items     *List   "forward:Add"
+	prefix    string  "prefix"
+	ZeroDelta bool    "optional"
 }
 
 func NewOffset(items ...Primitive) *Offset {
@@ -26,16 +27,30 @@ func NewOffset(items ...Primitive) *Offset {
 	return ret
 }
 
+func NewZeroOffset(items ...Primitive) *Offset {
+	ret := &Offset{
+		Items:     NewList(),
+		ZeroDelta: true,
+	}
+	ret.Items.SetParent(ret)
+	ret.Items.Add(items...)
+	return ret
+}
+
 func (o *Offset) Render(w *bufio.Writer) {
 	w.WriteString(o.Prefix())
 	w.WriteString("offset(")
 	if o.R != 0 {
 		w.WriteString(fmt.Sprintf("r = %f", o.R))
-	} else if o.Delta != 0 {
+	}
+	if o.Delta != 0 {
 		w.WriteString(fmt.Sprintf("delta = %f", o.Delta))
 		if o.Chamfer {
 			w.WriteString(fmt.Sprintf(", chamfer=%t", o.Chamfer))
 		}
+	}
+	if o.ZeroDelta {
+		w.WriteString(fmt.Sprintf("delta = 0"))
 	}
 	w.WriteString(")")
 	o.Items.Render(w)
